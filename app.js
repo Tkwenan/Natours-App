8cf461372e44417cff05bec6ed728d89adcea57c
Tracy Kwena <54999162+Tkwenan@users.noreply.github.com>
2023-01-23 13:28:57 -0500

    Integrate Stripe payments on the backend

--- app.js
+++ app.js
@@ -12,6 +12,7 @@ const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
+const bookingRouter = require('./routes/bookingRoutes');
 const reviewRouter = require('./routes/reviewRoutes');
 const viewRouter = require('./routes/viewRoutes');
 
@@ -110,6 +111,7 @@ app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
+app.use('/api/v1/bookings', bookingRouter);
 
 //app.all
 //we specify the route in the (). The * stands for all routes
